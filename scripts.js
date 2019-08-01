f9680f881a356dfc572d2373ca26cc738391182b
Shazin R <shazin.ramji@gmail.com>
2019-07-31 23:54:00 -0400

    add illustrations and styling

--- scripts.js
+++ scripts.js
@@ -1,173 +1,227 @@
-// Search Filters
-let country = document.getElementById('country-filters');
-let category = document.getElementById('category-filters');
-let countryArr = [
-    { 'countryName': 'Argentina', 'code': 'ar' },
-    { 'countryName': 'Australia', 'code': 'au' },
-    { 'countryName': 'Austria', 'code': 'at' },
-    { 'countryName': 'Belgium', 'code': 'be' },
-    { 'countryName': 'Brazil', 'code': 'br' },
-    { 'countryName': 'Bulgaria', 'code': 'bg' },
-    { 'countryName': 'Canada', 'code': 'ca' },
-    { 'countryName': 'China', 'code': 'cn' },
-    { 'countryName': 'Colombia', 'code': 'co' },
-    { 'countryName': 'Cuba', 'code': 'cu' },
-    { 'countryName': 'Czech Republic', 'code': 'cz' },
-    { 'countryName': 'Egypt', 'code': 'eg' },
-    { 'countryName': 'France', 'code': 'fr' },
-    { 'countryName': 'Germany', 'code': 'de' },
-    { 'countryName': 'Hong Kong', 'code': 'hk' },
-    { 'countryName': 'Hungary', 'code': 'hu' },
-    { 'countryName': 'India', 'code': 'in' },
-    { 'countryName': 'Indonesia', 'code': 'id' },
-    { 'countryName': 'Ireland', 'code': 'ie' },
-    { 'countryName': 'Israel', 'code': 'il' },
-    { 'countryName': 'Italy', 'code': 'it' },
-    { 'countryName': 'Japan', 'code': 'jp' },
-    { 'countryName': 'Korea, Republic of', 'code': 'kr' },
-    { 'countryName': 'Lithuania', 'code': 'lt' },
-    { 'countryName': 'Latvia', 'code': 'lv' },
-    { 'countryName': 'Malaysia', 'code': 'my' }, 
-    { 'countryName': 'Mexico', 'code': 'mx' },
-    { 'countryName': 'Morocco', 'code': 'ma' },
-    { 'countryName': 'Netherlands', 'code': 'nl' },
-    { 'countryName': 'Nigeria', 'code': 'ng' },
-    { 'countryName': 'New Zealand', 'code': 'nz' },
-    { 'countryName': 'Norway', 'code': 'no' },
-    { 'countryName': 'Philippines', 'code': 'ph' },
-    { 'countryName': 'Poland', 'code': 'pl' },
-    { 'countryName': 'Portugal', 'code': 'pt' },
-    { 'countryName': 'Romania', 'code': 'ro' },
-    { 'countryName': 'Russian Federation', 'code': 'ru' },
-    { 'countryName': 'Saudi Arabia', 'code': 'sa' },
-    { 'countryName': 'Serbia', 'code': 'rs' },
-    { 'countryName': 'Singapore', 'code': 'sg' },
-    { 'countryName': 'Slovakia', 'code': 'sk' },
-    { 'countryName': 'South Africa', 'code': 'za' },
-    { 'countryName': 'Sweden', 'code': 'se' },
-    { 'countryName': 'Switzerland', 'code': 'ch' },
-    { 'countryName': 'Taiwan, Province of China', 'code': 'tw' },
-    { 'countryName': 'Thailand', 'code': 'th' },
-    { 'countryName': 'Turkey', 'code': 'tr' },
-    { 'countryName': 'Ukraine', 'code': 'ua' },    
-    { 'countryName': 'United Arab Emirates', 'code': 'ae' }, 
-    { 'countryName': 'United Kingdom', 'code': 'gb' },                           
-    { 'countryName': 'US', 'code': 'us' },
-    { 'countryName': 'Venezuela', 'code': 've' }
-];
-let categoryArr = ['Business', 'Entertainment', 'General', 'Health', 'Science', 'Sports', 'Technology'];
+(function(){
 
-// Creates filter dropdown menus
-countryArr.map(countryItem => {
-    var countOpt = document.createElement('option');
-    countOpt.innerHTML = countryItem.countryName;
-    countOpt.value = countryItem.code;
-    country.appendChild(countOpt);
-});
-categoryArr.map(categoryItem => {
-    var catOpt = document.createElement('option');
-    catOpt.innerHTML = categoryItem;
-    catOpt.value = categoryItem;
-    category.appendChild(catOpt);
-});
-
-let navCount = 0;    
-
-controllerFunc = (dataArticles) => {
-    controlStyling = (dir,trans) => {
-        dir.style.opacity = trans;
+    function createID(name) {
+        return document.getElementById(name);
     }
 
-    if(navCount <= 0) {
-        controlStyling(left, 0.5);
-        navCount = 0;
-    } else {
-        controlStyling(left, 1);
-    }
-    if(navCount >= dataArticles.length - 1) {
-        controlStyling(right, 0.5);
-        navCount = dataArticles.length - 1;
-    } else {
-        controlStyling(right, 1);
-    }    
-}
+    var remoteScreen = document.getElementById('remote-screen');
+    var article = createID('article');
+    var author = createID('author');
+    var playNewsFeed = createID('play-newsfeed');
 
-createArticles = (dataArticles) => {
-    controllerFunc(dataArticles);
-    document.getElementById('article-image').src = dataArticles[navCount].urlToImage;
-    document.getElementById('title').innerHTML = dataArticles[navCount].title;
-    document.getElementById('description').innerText = dataArticles[navCount].description;
-    document.getElementById('author').innerText = dataArticles[navCount].author;
-    document.getElementById('source').innerText = dataArticles[navCount].source.name;
-    document.getElementById('date').innerText = new Date(Date.parse(dataArticles[navCount].publishedAt)).toDateString();
-    document.getElementById('link').href = dataArticles[navCount].url;    
-}
+    var power = createID('power');
+    var powerBtn = power.querySelector('img');
+    powerBtn.addEventListener('click', function() {
+        power.classList.add('hidden');
+        remoteScreen.className = 'show';  
+        createID('tv-remote').classList.add('slide-up');
+        typingTitle(showRemoteTitle);
+    })
 
-fetchNewsData = (countVal, catVal) => {
-    let url = 'https://newsapi.org/v2/top-headlines?' +
-    'country=' + countVal + '&' +
-    'category=' + catVal + '&' +
-    'apiKey=c951460373084c91bdf4fa34f4b4c44c';
+    // Search Filters
+    let country = createID('country-filters');
+    let category = createID('category-filters');
+    let countryArr = [
+        { 'countryName': 'Argentina', 'code': 'ar' },
+        { 'countryName': 'Australia', 'code': 'au' },
+        { 'countryName': 'Austria', 'code': 'at' },
+        { 'countryName': 'Belgium', 'code': 'be' },
+        { 'countryName': 'Brazil', 'code': 'br' },
+        { 'countryName': 'Bulgaria', 'code': 'bg' },
+        { 'countryName': 'Canada', 'code': 'ca' },
+        { 'countryName': 'China', 'code': 'cn' },
+        { 'countryName': 'Colombia', 'code': 'co' },
+        { 'countryName': 'Cuba', 'code': 'cu' },
+        { 'countryName': 'Czech Republic', 'code': 'cz' },
+        { 'countryName': 'Egypt', 'code': 'eg' },
+        { 'countryName': 'France', 'code': 'fr' },
+        { 'countryName': 'Germany', 'code': 'de' },
+        { 'countryName': 'Hong Kong', 'code': 'hk' },
+        { 'countryName': 'Hungary', 'code': 'hu' },
+        { 'countryName': 'India', 'code': 'in' },
+        { 'countryName': 'Indonesia', 'code': 'id' },
+        { 'countryName': 'Ireland', 'code': 'ie' },
+        { 'countryName': 'Israel', 'code': 'il' },
+        { 'countryName': 'Italy', 'code': 'it' },
+        { 'countryName': 'Japan', 'code': 'jp' },
+        { 'countryName': 'Korea, Republic of', 'code': 'kr' },
+        { 'countryName': 'Lithuania', 'code': 'lt' },
+        { 'countryName': 'Latvia', 'code': 'lv' },
+        { 'countryName': 'Malaysia', 'code': 'my' }, 
+        { 'countryName': 'Mexico', 'code': 'mx' },
+        { 'countryName': 'Morocco', 'code': 'ma' },
+        { 'countryName': 'Netherlands', 'code': 'nl' },
+        { 'countryName': 'Nigeria', 'code': 'ng' },
+        { 'countryName': 'New Zealand', 'code': 'nz' },
+        { 'countryName': 'Norway', 'code': 'no' },
+        { 'countryName': 'Philippines', 'code': 'ph' },
+        { 'countryName': 'Poland', 'code': 'pl' },
+        { 'countryName': 'Portugal', 'code': 'pt' },
+        { 'countryName': 'Romania', 'code': 'ro' },
+        { 'countryName': 'Russian Federation', 'code': 'ru' },
+        { 'countryName': 'Saudi Arabia', 'code': 'sa' },
+        { 'countryName': 'Serbia', 'code': 'rs' },
+        { 'countryName': 'Singapore', 'code': 'sg' },
+        { 'countryName': 'Slovakia', 'code': 'sk' },
+        { 'countryName': 'South Africa', 'code': 'za' },
+        { 'countryName': 'Sweden', 'code': 'se' },
+        { 'countryName': 'Switzerland', 'code': 'ch' },
+        { 'countryName': 'Taiwan, Province of China', 'code': 'tw' },
+        { 'countryName': 'Thailand', 'code': 'th' },
+        { 'countryName': 'Turkey', 'code': 'tr' },
+        { 'countryName': 'Ukraine', 'code': 'ua' },    
+        { 'countryName': 'United Arab Emirates', 'code': 'ae' }, 
+        { 'countryName': 'United Kingdom', 'code': 'gb' },                           
+        { 'countryName': 'US', 'code': 'us' },
+        { 'countryName': 'Venezuela', 'code': 've' }
+    ];
+    let categoryArr = ['Business', 'Entertainment', 'General', 'Health', 'Science', 'Sports', 'Technology'];
 
-    fetch(new Request(url))
-        .then(function(response) {
-        response.json().then(function(data) {
-            let dataArticles = data.articles;
-            createArticles(dataArticles);
-        });
+    countryArr = countryArr.map(function(countryItem) {
+        var countOpt = document.createElement('option');
+        countOpt.innerHTML = countryItem.countryName;
+        countOpt.value = countryItem.code;
+        country.appendChild(countOpt);
     });
-};
 
-// Typing text header
-let titleCount = 0;
-let title = 'The Latest Headlines';
-typingTitle = () => {
-    if(titleCount < title.length) {
-        document.getElementsByTagName('h1')[0].innerHTML += title.charAt(titleCount);
-        titleCount++;
-        setTimeout(typingTitle, 200);
+    categoryArr = categoryArr.map(function(categoryItem) {
+        var catOpt = document.createElement('option');
+        catOpt.innerHTML = categoryItem;
+        catOpt.value = categoryItem;
+        category.appendChild(catOpt);
+    });    
+
+    let navCount = 0;
+
+    controllerFunc = function(dataArticles) {
+        controlStyling = function(dir,trans) {
+            dir.style.opacity = trans;
+        }
+        if(navCount <= 0) {
+            controlStyling(left, 0.5);
+            navCount = 0;
+        } else {
+            controlStyling(left, 1);
+        }
+        if(navCount >= dataArticles.length - 1) {
+            controlStyling(right, 0.5);
+            navCount = dataArticles.length - 1;
+        } else {
+            controlStyling(right, 1);
+        }    
+    }    
+
+    createArticles = function(dataArticles) {
+        controllerFunc(dataArticles);
+        article.querySelector('img').src = dataArticles[navCount].urlToImage;
+        var titlStr = dataArticles[navCount].title.substring(0, 50);
+        createID('title').innerHTML = titlStr.substring(0, Math.min(titlStr.length, titlStr.lastIndexOf(" "))) + ' ...';
+        var descStr = dataArticles[navCount].description.substring(0, 80);
+        createID('description').innerText = descStr.substring(0, Math.min(descStr.length, descStr.lastIndexOf(" "))) + ' ...';
+        author.innerText = dataArticles[navCount].author;
+        if(author.innerText == '') {
+            document.querySelector('.dash').className = 'hidden';
+        } else {
+            document.querySelector('.dash').style.display = 'inline-block';
+        }
+        
+        createID('source').innerText = dataArticles[navCount].source.name;
+        createID('date').innerText = new Date(Date.parse(dataArticles[navCount].publishedAt)).toDateString();
+        article.querySelector('.link').href = dataArticles[navCount].url;
+        playNewsFeed.querySelector('.link').href = dataArticles[navCount].url;
+        createID('center-content').querySelector('.link').href = dataArticles[navCount].url;        
+        createID('bottom-content').querySelector('.link').href = dataArticles[navCount].url;
     }
-};  
 
-// Gets Filtered data and runs api
-getData = () => {
-    let countVal = country.value;
-    let catVal = category.value;    
+    fetchNewsData = function(countVal, catVal) {
+        let url = 'https://newsapi.org/v2/top-headlines?' +
+        'country=' + countVal + '&' +
+        'category=' + catVal + '&' +
+        'apiKey=c951460373084c91bdf4fa34f4b4c44c';
 
-    // Sets default country value to Canada
-    if(countVal == '') countVal = country.value = 'ca';
-    // Sets default category value to General
-    if(catVal == '') catVal = category.value = 'General';
+        fetch(new Request(url))
+            .then(function(response) {
+            response.json().then(function(data) {
+                let dataArticles = data.articles;
+                createArticles(dataArticles);
+            });
+        });
+    };
 
-    fetchNewsData(countVal, catVal);
-}
+    showRemoteTitle = function() {
+        remoteScreen.querySelector('p').style.opacity = 1;
+    }     
 
-let btn = document.getElementById('search-filters').querySelector('button');
-let newsFeed = document.getElementById('news-feed');
-btn.addEventListener('click', function() {    
-    newsFeed.style.display = 'block';
-    getData();
-    typingTitle();
-});
+    // Typing text header
+    let titleCount = 0;
+    let title = 'Read the Latest Headlines';
+    typingTitle = function(showRemoteTitle) {
+        if(titleCount < title.length) {                        
+            remoteScreen.querySelector('h2').innerHTML += title.charAt(titleCount);
+            titleCount++;
+            setTimeout(typingTitle, 70);
+        }
+        setTimeout(showRemoteTitle, 1750);
+    };
 
-// Controller Navigation
-var controller = document.getElementById('controller');
+    // Gets Filtered data and runs api
+    getData = function() {
+        let countVal = country.value;
+        let catVal = category.value;    
 
-var left = document.createElement('div');
-left.id = "left"
-left.innerHTML = '&lBarr;';
-controller.appendChild(left);
+        // Sets default country value to Canada
+        if(countVal == '') countVal = country.value = 'ca';
+        // Sets default category value to General
+        if(catVal == '') catVal = category.value = 'General';
 
-var right = document.createElement('div');
-right.id = "right"
-right.innerHTML = '&rBarr;';
-controller.appendChild(right);   
+        fetchNewsData(countVal, catVal);
+    }
 
-left.onclick = () => {
-    navCount--;
-    getData();
-}
-right.onclick = () => {
-    navCount++;
-    getData();
-}
+    var controller = createID('controller');
+    let btn = createID('search-filters').querySelector('button');
+    btn.addEventListener('click', function() {    
+        playNewsFeed.className = 'show';
+        controller.style.display = 'flex';
+        remoteScreen.className = 'hidden';
+        getData();        
+    });
+    btn.onmousedown = function() {
+        btn.classList.add('inset-shadow');
+    }
+    btn.onmouseup = function() {
+        btn.classList.remove('inset-shadow');
+    }  
+
+    // Controller Navigation
+    var left = document.createElement('div');
+    left.id = "left"
+    left.innerHTML = '&larr;';
+    controller.appendChild(left);
+
+    var right = document.createElement('div');
+    right.id = "right"
+    right.innerHTML = '	&rarr;';
+    controller.appendChild(right);   
+
+    left.onclick = function() {        
+        navCount--;
+        getData();
+    }
+    right.onclick = function() {
+        navCount++;
+        getData();
+    }
+    left.onmousedown = function() {
+        left.classList.add('inset-shadow');
+    }
+    right.onmousedown = function() {
+        right.classList.add('inset-shadow');
+    }    
+    left.onmouseup = function() {
+        left.classList.remove('inset-shadow');
+    }
+    right.onmouseup = function() {
+        right.classList.remove('inset-shadow');
+    }
+    
+}());
